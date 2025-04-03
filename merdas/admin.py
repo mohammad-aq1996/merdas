fe8fe86b0db9eb6233bb63b57c0de01a5bf20094
mohammad-aq1996 <mohammad.aq1996@yahoo.com>
2025-04-03 17:19:29 +0330

    [add] merdas: add Standard, SR and FR model

--- merdas/admin.py
+++ merdas/admin.py
@@ -4,3 +4,6 @@ from .models import *
 
 admin.site.register(Organization)
 admin.site.register(OrganizationType)
+admin.site.register(Standard)
+admin.site.register(SR)
+admin.site.register(FR)
--- merdas/models.py
+++ merdas/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from django.core.exceptions import ValidationError
+from core.models import BaseModel
 
 
 class OrganizationType(models.Model):
@@ -25,3 +27,38 @@ class Organization(MPTTModel):
 
     def __str__(self):
         return self.name
+
+
+class SR(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class FR(BaseModel):
+    title = models.CharField(max_length=255)
+    weight = models.PositiveIntegerField()
+    description = models.TextField(blank=True, null=True)
+    sr = models.ForeignKey(SR, on_delete=models.CASCADE, related_name='frs')
+
+    def __str__(self):
+        return self.title
+
+
+class Standard(BaseModel):
+    title = models.CharField(max_length=255)
+    fr = models.ForeignKey(FR, on_delete=models.CASCADE, related_name='standards')
+
+    def clean(self):
+        total_weight = sum(fr.weight for fr in FR.objects.filter(standard=self))
+        if total_weight != 100:
+            raise ValidationError('جمع وزن‌های FR برای هر استاندارد باید ۱۰۰ باشد.')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
