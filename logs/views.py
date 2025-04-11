8dc5277cc4453405bc29971fc3b709ae08c7d136
mohammad-aq1996 <mohammad.aq1996@yahoo.com>
2025-04-11 13:17:24 +0330

    [add] add use username to logs serializer

--- logs/views.py
+++ logs/views.py
@@ -8,15 +8,20 @@ from .models import *
 
 
 class LogSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = EventLog
         fields = '__all__'
+
+    def get_user(self, obj):
+        return obj.user.username if obj.user else None
 
 
 class LogView(APIView):
     queryset = EventLog.objects.all()
 
     def get(self, request):
-        logs = EventLog.objects.all()
+        logs = EventLog.objects.all().order_by('-created_at')
         serializer = LogSerializer(logs, many=True)
         return CustomResponse.success(message=get_all_data(), data=serializer.data)
