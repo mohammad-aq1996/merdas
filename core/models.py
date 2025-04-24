587765b009adeb911bdc4d5e07af854e361df60d
mohammad-aq1996 <mohammad.aq1996@yahoo.com>
2025-04-24 18:59:21 +0330

    [add] add uuid to base model

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 import django_jalali.db.models as jmodels
 from django.db import models
+import uuid
 
 
 class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     updated_at = jmodels.jDateTimeField(auto_now=True)
 
