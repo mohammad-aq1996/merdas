2179c51e0bd16be8acfe98ef57136ab4d12fdc49
mohammad-aq1996 <mohammad.aq1996@yahoo.com>
2025-05-05 21:27:41 +0330

    [add] Refresh Token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
-
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('logout/', LogoutView.as_view(), name='logout'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('admin-change-password/', AdminChangePasswordView.as_view(), name='admin-change-password'),
